3434ac1dda68bd67ca0593ddd6497bfa24058b16
yoskary80@gmail.com <yoskary80@gmail.com>
2023-10-25 22:24:02 -0300

    MyhappyCake

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import './Style.css'
+
+function Home() {
+    return (
+        <div>
+            <h1>Bienvenid@ a Happy Cake!</h1>
+            <h4>El lugar de los pasteles felices</h4>
+            <img src="https://annaspasteleria.com/images/_imageBlock/Untitled-2wbbb.jpg" className="imagen"/>
+        </div>
+    )
+}
+
+export default Home;
